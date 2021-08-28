c5e6c444cb9ca7f2f0835e0f6e71adb61ebea356
lenguyenqd <nguyenquocdaile@gmail.com>
2021-08-28 17:19:27 +0700

    improve UI

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -3,54 +3,60 @@ import CartItem from '../cart-item';
 import Header from '../header';
 import './index.scss'
 
-const Cart = ({cartItems, addToCart, removeFromCart }) => {
-  const calculateTotal = (items) =>items.reduce((ack, item) => ack + item.amount * item.price, 0);
+const Cart = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items) => items.reduce((ack, item) => ack + item.amount * item.price, 0);
   return (
     <>
-      <Header isBackroundOrange={true}/>
+      <Header isBackroundOrange={true} />
       <div className="cart">
         <div className="cart-header">
           Giỏ hàng
         </div>
         <p className="detail-text">Chi tiết ơn hàng</p>
-        {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+        {cartItems.length === 0 ? <p style={{ textAlign: 'center', fontSize: '1.6rem' }}>No items in cart.</p> : null}
         <div className="cart-body">
-            {
-              cartItems.map(item =>  
-                <CartItem
-                  key={item.id}
-                  item={item}
-                  addToCart={addToCart}
-                  removeFromCart={removeFromCart} 
-                />
-              )
-            }
+          {
+            cartItems.map(item =>
+              <CartItem
+                key={item.id}
+                item={item}
+                addToCart={addToCart}
+                removeFromCart={removeFromCart}
+              />
+            )
+          }
         </div>
-        <div className="prices">
-          <div className="prices__item">
-            <span>Tạm tính</span>
-            <span style={{fontWeight: '700'}}>100.000 đ</span>
-          </div>
-          <div className="prices__item">
-            <span>Phí vận chuyển</span>
-            <span style={{fontWeight: '700'}}>15.000 đ</span>
-          </div>
-          <div className="prices__item">
-            <span>Tổng cộng</span>
-            <span style={{fontWeight: '700'}}>115.000 đ</span>
-          </div>
-        </div>
-        <div className="price-pay">
-          <div className="prices__item">
-            <span>Tổng cộng</span>
-            <span style={{fontWeight: '700'}}>115.000 đ</span>
-          </div>
-          <button className="pay-button">Thanh toán</button>
-        </div>
+        {
+          cartItems.length > 0 ? (
+            <>
+              <div className="prices">
+                <div className="prices__item">
+                  <span>Tạm tính</span>
+                  <span style={{ fontWeight: '700' }}>100.000 đ</span>
+                </div>
+                <div className="prices__item">
+                  <span>Phí vận chuyển</span>
+                  <span style={{ fontWeight: '700' }}>15.000 đ</span>
+                </div>
+                <div className="prices__item">
+                  <span>Tổng cộng</span>
+                  <span style={{ fontWeight: '700' }}>115.000 đ</span>
+                </div>
+              </div>
+              <div className="price-pay">
+                <div className="prices__item">
+                  <span>Tổng cộng</span>
+                  <span style={{ fontWeight: '700' }}>115.000 đ</span>
+                </div>
+                <button className="pay-button">Thanh toán</button>
+              </div>
+            </>
+          ) : null
+        }
       </div>
     </>
   )
-} 
+}
 
 export default Cart
 
